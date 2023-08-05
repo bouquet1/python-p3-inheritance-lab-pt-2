93cf4745fe710b422651fdae54a65e1742e141f3
bouquet1 <bukuleta@gmail.com>
2023-08-04 18:49:36 -0700

    passed

--- lib/student.py
+++ lib/student.py
@@ -7,4 +7,12 @@ class Student:
         print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+         for count in range(10):
+             super().raise_hand()
+        
